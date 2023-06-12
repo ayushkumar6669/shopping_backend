4ce478e81eed6343dcf85c73ddbe69ef1bd7ecb8
AYUSH KUMAR <ayush.kumar@arthmate.com>
2023-06-12 17:53:56 +0530

    check dedupe

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const { validationResult } = require('express-validator');
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
 
 exports.register = async (req, res) => {
   const errors = validationResult(req);
@@ -10,17 +10,25 @@ exports.register = async (req, res) => {
 
   const { username, email, password } = req.body;
 
+  let user = await User.findOne({ $or: [{ username }, { email }] });
+
+  if (user) {
+    return res
+      .status(400)
+      .json({ message: "Username or email already exists" });
+  }
+
   try {
     const user = new User({ username, email, password });
     await user.save();
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '1h',
+      expiresIn: "1h",
     });
 
     res.status(201).json({ token });
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error", error });
   }
 };
 
@@ -35,20 +43,20 @@ exports.login = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: 'Invalid email or password' });
+      return res.status(400).json({ message: "Invalid email or password" });
     }
 
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid email or password' });
+      return res.status(400).json({ message: "Invalid email or password" });
     }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '1h',
+      expiresIn: "1h",
     });
 
     res.status(200).json({ token });
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 };
