709fbbd05c116aafaf866386e86a085bee8ed093
AYUSH KUMAR <ayush.kumar@arthmate.com>
2023-06-13 00:15:24 +0530

    added password length constraint

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,23 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { check } = require('express-validator');
-const authController = require('../controllers/authController');
+const { check } = require("express-validator");
+const authController = require("../controllers/authController");
 
 router.post(
-  '/register',
+  "/register",
   [
-    check('username', 'Username is required').notEmpty(),
-    check('email', 'Email is required').isEmail(),
-    check('password', 'Password is required').isLength({ min: 6 }),
+    check("username", "Username is required").notEmpty(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters long"),
   ],
   authController.register
 );
 
 router.post(
-  '/login',
+  "/login",
   [
-    check('email', 'Email is required').isEmail(),
-    check('password', 'Password is required').notEmpty(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required").notEmpty(),
   ],
   authController.login
 );
