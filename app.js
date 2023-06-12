1ce71e73f89628db8182bb6cccc6e6a1b339d104
AYUSH KUMAR <ayush.kumar@arthmate.com>
2023-06-12 16:20:22 +0530

    resolved mongo error

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
+  // useCreateIndex: true,
 });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Database connected successfully");
+})
 
 app.use(cors());
 app.use(bodyParser.json());
